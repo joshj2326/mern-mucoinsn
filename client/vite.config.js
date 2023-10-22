b9d7072ac9f1af1a51e5e6bed91e26e21a132142
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-22 07:23:25 +0530

    create header components

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174, // Change this to the desired port number (e.g., 5174)
+  },
 })
+
+
