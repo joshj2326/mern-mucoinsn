5315f5b1eaee57a5094a1d75b5f9dc5560b34307
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-23 20:18:28 +0530

    create user update api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -53,7 +53,7 @@ export default function Profile() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-3xl font-semibold text-center my-7'> Management Profile</h1>
       <form className='flex flex-col gap-4'>
 
         <input
