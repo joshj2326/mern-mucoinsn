952e92aae11ebadc0304bef7f59e93527870971c
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-24 12:27:50 +0530

    complete create listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -6,15 +6,30 @@ import {
     uploadBytesResumable,
   } from 'firebase/storage';
 import { app } from '../firebase';
+import {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
-    
+    name: '',
+    description: '',
+    category: '',
+    all: false,
+    computer: false,
+    it: false,
+    mechanical: false,
+    extc: false,
+    electrical: false,
+    rewardvalue: 0,
   });
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 2) {
@@ -77,13 +92,80 @@ export default function CreateListing() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
+
+  const handleChange= (e) => { 
+    if (
+      e.target.id === 'all' ||
+      e.target.id === 'computer' ||
+      e.target.id === 'it' ||
+      e.target.id === 'mechanical' ||
+      e.target.id === 'extc' ||
+      e.target.id === 'electrical'
+    ) {
+      if (e.target.id === 'all') {
+        setFormData({
+          ...formData,
+          [e.target.id]: e.target.checked,
+          computer: e.target.checked,
+          it: e.target.checked,
+          mechanical: e.target.checked,
+          extc: e.target.checked,
+          electrical: e.target.checked,
+        });
+      } else {
+        setFormData({
+          ...formData,
+          [e.target.id]: e.target.checked,
+        });
+      }
+    }
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('You must upload at least one image');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
  
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
         Create Tasks
       </h1>
-      <form className='flex flex-col sm:flex-row gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
           <input
             type='text'
@@ -93,12 +175,16 @@ export default function CreateListing() {
             maxLength='62'
             minLength='5'
             required
+            onChange={handleChange}
+            value={formData.name}
           />
           <textarea
             placeholder='Description'
             className='border p-3 rounded-lg'
             id='description'
             required
+            onChange={handleChange}
+            value={formData.description}
           />
           <input
             type='text'
@@ -106,31 +192,45 @@ export default function CreateListing() {
             className='border p-3 rounded-lg'
             id='category'
             required
+            onChange={handleChange}
+            value={formData.category}
           />
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
               <span>Branches :</span>
-              <input type='checkbox' id='all' className='w-5' />
+              <input type='checkbox' id='all' className='w-5'
+              onChange={handleChange}
+              checked={formData.all}/>
               <span>All</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='computer' className='w-5' />
+              <input type='checkbox' id='computer' className='w-5'
+              onChange={handleChange}
+              checked={formData.computer} />
               <span>Computer</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='it' className='w-5' />
+              <input type='checkbox' id='it' className='w-5'
+              onChange={handleChange}
+              checked={formData.it} />
               <span>IT</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='mechanical' className='w-5' />
+              <input type='checkbox' id='mechanical' className='w-5'
+              onChange={handleChange}
+              checked={formData.mechanical} />
               <span>Mechanical</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='extc' className='w-5' />
+              <input type='checkbox' id='extc' className='w-5'
+              onChange={handleChange}
+              checked={formData.extc} />
               <span>EXTC</span>
             </div>
             <div className='flex gap-2'>
-              <input type='checkbox' id='electrical' className='w-5' />
+              <input type='checkbox' id='electrical' className='w-5'
+              onChange={handleChange}
+              checked={formData.electrical} />
               <span>Electrical</span>
             </div>
           </div>
@@ -143,7 +243,8 @@ export default function CreateListing() {
               max='100'
               required
               className='p-3 border border-gray-300 rounded-lg'
-            />
+              onChange={handleChange}
+              checked={formData.rewardvalue}/>
             <div className='flex flex-col items-center'>
               <p>Reward Value</p>
               <span className='text-xs'>(ETH)</span>
@@ -189,7 +290,8 @@ export default function CreateListing() {
                 </div>
                 ))
           }
-          <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Task</button>
+          <button disabled={loading || uploading} className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Creating...' : 'Create task'}</button>
+          {error && <p className='text-red-700 text-sm'>{error}</p>}
           </div>
       </form>
     </main>
