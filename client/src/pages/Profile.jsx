1f9ef8ac3d5fce60835b0cc2867028ddaa69584f
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-24 06:19:40 +0530

    complete upload listing images functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import {
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserSuccess, deleteUserStart, signOutUserStart } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -178,6 +179,9 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
         {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to ={"/create-listing"}>
+         Create tasks
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
