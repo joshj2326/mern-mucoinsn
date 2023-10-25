858e65d9b5d9c3d143ed53261f7c352c42797b0b
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-25 11:48:54 +0530

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About μCoins</h1>
+      <p className='mb-4 text-slate-700'>A decentralized rewards system aiming to incentivize students for optional
+academic goals and active participation in university routines and extracurricular activities. The system
+employs a cryptocurrency-based framework, benefiting both students and educational institutions.</p>
+      <p className='mb-4 text-slate-700'>
+      By
+earning rewards, students find motivation to persist in academics, fostering positivity and driving efforts
+toward goal achievement. Drawing inspiration from rewards' impact on behavior, the project aims to
+enhance academic motivation and performance. Methodologies include surveys and consultations with
+university management to identify deserving tasks and reward expectations. Rigorous research on
+cryptocurrency economies ensures μCoins' viability.
+      </p>
+      <p className='mb-4 text-slate-700'>Initially piloted at the college level, the system's
+effectiveness is assessed for potential expansion. Outcomes highlight μCoins' potential to amplify academic
+capabilities and expedite goal attainment, benefiting students and institutions. The research holds
+implications for motivation studies and innovative incentive systems in academia and industries. In
+conclusion, μCoins promises to revolutionize academic motivation and performance, inspiring further
+incentive-based learning research.</p>
     </div>
   )
 }
