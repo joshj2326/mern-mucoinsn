dbaa36407a66853a0dbe299d5ff1a1f8c68b5ef1
Joshj1601 <91051085+Joshj1601@users.noreply.github.com>
2023-10-25 11:42:29 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import SwiperCore from 'swiper';
+import 'swiper/css/bundle';
+import ListingItem from '../components/ListingItem';
 
 export default function Home() {
+  const [listings, setListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(listings);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?limit=4');
+        const data = await res.json();
+        setListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchListings();
+  }, []);
+
   return (
     <div>
-      Home
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Promoting <span className='text-slate-500'>Academic Achievement</span>
+          <br />
+          
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          A decentralized cryptocurrency based rewards system
+          <br />
+          Promoting academic achievement among students 
+        </div>
+        <Link to={'/search'} className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'>
+          Let's get started...
+        </Link>
+      </div>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {listings &&
+          listings.length > 0 &&
+          listings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[500px]'
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for all listings */}
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {listings && listings.length > 0 && (
+          <div>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent Tasks</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search'}>
+                Show more tasks
+              </Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {listings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
